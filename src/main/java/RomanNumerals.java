366eda76494bec616340c13786f972d5619b5db3
Egor18032019 <goro4@mail.ru>
2022-08-01 21:32:50 +0500

    exam

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -5,40 +5,6 @@ import java.util.regex.Pattern;
 
 public class RomanNumerals {
 
-    public final static class MyStaticClass {
-        static  Map<String, Integer> hundredsMap = new HashMap<String, Integer>(); // стобитная карта
-        static  Map<String, Integer> tensMap = new HashMap<String, Integer>(); // Десятибитная карта
-        static  Map<String, Integer> bitsMap = new HashMap<String, Integer>(); // Однобитовая карта
-
-        private MyStaticClass() {
-            String[] str1 = "C=100,CC=200,CCC=300,CD=400,D=500,DC=600,DCC=700,DCCC=800,CM=900".split(",");
-            for (String split : str1) {
-                String[] pair = split.split("=");
-                hundredsMap.put(pair[0], Integer.parseInt(pair[1]));
-            }
-
-            String[] str2 = "X=10,XX=20,XXX=30,XL=40,L=50,LX=60,LXX=70,LXXX=80,XC=90".split(",");
-            for (String split : str2) {
-                String[] pair = split.split("=");
-                tensMap.put(pair[0], Integer.parseInt(pair[1]));
-            }
-
-            String[] str3 = "I=1,II=2,III=3,IV=4,V=5,VI=6,VII=7,VIII=8,IX=9".split(",");
-            for (String split : str3) {
-                String[] pair = split.split("=");
-                bitsMap.put(pair[0], Integer.parseInt(pair[1]));
-            }
-        }
-    }
-    private RomanNumerals() {
-        MyStaticClass class1= new MyStaticClass();
-    }
-    public static void main(String[] args) {
-
-        System.out.println(RomanNumerals.toRoman(1000));
-        System.out.println(RomanNumerals.fromRoman("D"));
-    }
-
     public static String toRoman(int n) {
         return "I";
     }
@@ -49,6 +15,28 @@ public class RomanNumerals {
 
     private static int toArabic(String romaNum) {
         int result = 0; // Возвращенный результат
+
+        Map<String, Integer> hundredsMap = new HashMap<String, Integer>(); // стобитная карта
+        Map<String, Integer> tensMap = new HashMap<String, Integer>(); // Десятибитная карта
+        Map<String, Integer> bitsMap = new HashMap<String, Integer>(); // Однобитовая карта
+
+        String[] str1 = "C=100,CC=200,CCC=300,CD=400,D=500,DC=600,DCC=700,DCCC=800,CM=900".split(",");
+        for (String split : str1) {
+            String[] pair = split.split("=");
+            hundredsMap.put(pair[0], Integer.parseInt(pair[1]));
+        }
+
+        String[] str2 = "X=10,XX=20,XXX=30,XL=40,L=50,LX=60,LXX=70,LXXX=80,XC=90".split(",");
+        for (String split : str2) {
+            String[] pair = split.split("=");
+            tensMap.put(pair[0], Integer.parseInt(pair[1]));
+        }
+
+        String[] str3 = "I=1,II=2,III=3,IV=4,V=5,VI=6,VII=7,VIII=8,IX=9".split(",");
+        for (String split : str3) {
+            String[] pair = split.split("=");
+            bitsMap.put(pair[0], Integer.parseInt(pair[1]));
+        }
 
         Pattern thousandsPattern = Pattern.compile("^M{0,3}");
         Pattern hundredsPattern = Pattern.compile("CM|CD|D?C{0,3}");
@@ -67,24 +55,51 @@ public class RomanNumerals {
         Matcher match2 = hundredsPattern.matcher(romaNum.substring(match1.end()));
         match2.find();
         if (!match2.group().equals(""))
-            result = result + MyStaticClass.hundredsMap.get(match2.group());
+            result = result + hundredsMap.get(match2.group());
 
         Matcher match3 = tensPattern.matcher(romaNum.substring(match2.end() + match1.end()));
         match3.find();
 
         if (!match3.group().equals("")) {
-            result = result + MyStaticClass.tensMap.get(match3.group());
+            result = result + tensMap.get(match3.group());
         }
         Matcher match4 = bitsPattern.matcher(romaNum.substring(match2.end() + match1.end() + match3.end()));
         match4.find();
         if (!match4.group().equals(""))
-            result = result + MyStaticClass.bitsMap.get(match4.group());
+            result = result + bitsMap.get(match4.group());
 
         return result;
     }
 
-    private static String toForRoman(int number) {
+    private static StringBuilder toFromRoman(int numbeer) {
 
-        return "d";
+        StringBuilder result = new StringBuilder(); // Возвращенный результат
+
+        Map<Integer,String > hundredsMap = new HashMap<Integer, String>(); // стобитная карта
+        Map<Integer, String> tensMap = new HashMap<Integer, String>(); // Десятибитная карта
+        Map<Integer, String> bitsMap = new HashMap<Integer, String>(); // Однобитовая карта
+
+        String[] str1 = "C=100,CC=200,CCC=300,CD=400,D=500,DC=600,DCC=700,DCCC=800,CM=900".split(",");
+        for (String split : str1) {
+            String[] pair = split.split("=");
+            hundredsMap.put(Integer.parseInt(pair[1]), pair[0]);
+        }
+
+        String[] str2 = "X=10,XX=20,XXX=30,XL=40,L=50,LX=60,LXX=70,LXXX=80,XC=90".split(",");
+        for (String split : str2) {
+            String[] pair = split.split("=");
+            tensMap.put(Integer.parseInt(pair[1]), pair[0]);
+        }
+
+        String[] str3 = "I=1,II=2,III=3,IV=4,V=5,VI=6,VII=7,VIII=8,IX=9".split(",");
+        for (String split : str3) {
+            String[] pair = split.split("=");
+            bitsMap.put(Integer.parseInt(pair[1]), pair[0]);
+        }
+
+// разложить числа на составляющие
+// добавить гит игнорёЁ
+
+        return result;
     }
 }
