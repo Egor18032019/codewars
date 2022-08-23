d8cb26d2ae5fd6551b563bcb25974a33ad65ad1e
Egor18032019 <goro4@mail.ru>
2022-08-23 17:28:41 +0500

    Задачи с leetcode

--- src/main/java/MyHashSet.java
+++ src/main/java/MyHashSet.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+//  таки и не понял смысл этого задание на лит коде.
+// или нельзя что ли было использовать ArrayList и List ?? только int[] ?
 public class MyHashSet {
+
+
+    //    private List<Integer>[] table = null;
+    private List<Integer> table = null;
+
+    public MyHashSet() {
+        table = new ArrayList<>();
+    }
+
+    public void add(int key) {
+        for (Integer point : table) {
+            if (key == point) return;
+        }
+        table.add(key);
+    }
+
+    public void remove(int key) {
+        int index = table.indexOf(key);
+        if (index != -1) table.remove(index);
+    }
+
+    public boolean contains(int key) {
+        return table.contains(key);
+    }
 }
